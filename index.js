2a0320db5344d5d3ea46939698a81304956116b8
Veronica Kim <vero.a.kim@gmail.com>
2017-11-23 18:17:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return("I can't hear you!");
-  } else if (string === string.toUpperCase())
+  } else if (string === string.toUpperCase()){
     return("YES INDEED!");
-  else (string === "I love you, Grandma.")
+  }else (string === "I love you, Grandma.")
     return("I love you, too");
 }
