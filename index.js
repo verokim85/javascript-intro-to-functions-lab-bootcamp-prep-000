ba4f0f77787e6ef81213cd2aedcbd7b25564ef0b
Veronica Kim <vero.a.kim@gmail.com>
2017-11-23 18:06:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,3 @@ function logShout(string) {
 function logWhisper(string) {
   console.log (string.toLowerCase())
 }
-
-function sayHiToGrandma(string) {
-  if string.toLowerCase = ("I can't hear you!");
-}
