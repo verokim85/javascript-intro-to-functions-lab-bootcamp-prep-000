41b3d0bd035619b7ffa7163bfb8cad65f1e354ed
Veronica Kim <vero.a.kim@gmail.com>
2017-11-23 18:20:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ function sayHiToGrandma(string) {
   } else if (string === string.toUpperCase()) {
     return("YES INDEED!");
   } else (string === "I love you, Grandma.") {
-    return("I love you, too");
+    return("I love you, too.");
 }}
